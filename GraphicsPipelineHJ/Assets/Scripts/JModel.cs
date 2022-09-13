1fc5c842eac370f5bc93c5a33a71e7829401870b
HydenJohnson75 <78604065+ToxicOddesey@users.noreply.github.com>
2022-09-13 23:03:44 +0100

    Finished Top, Bottom, Left and Right Faces for Model and added Lists to JModel Script
    Signed-off-by: HydenJohnson75 <78604065+ToxicOddesey@users.noreply.github.com>

---
+++ GraphicsPipelineHJ/Assets/Scripts/JModel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JModel : MonoBehaviour
+{
+    List<Vector3> Vertices = new List<Vector3>();
+
+    List<Vector3Int> Faces = new List<Vector3Int>();
+}
